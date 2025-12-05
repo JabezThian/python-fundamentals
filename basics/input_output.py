061d35837e994f9ffc7de0ef9267d97d2a86e44a
JabezThian <thian.jabez@gmail.com>
2025-12-05 17:39:05 +0800

    adding input_output file

---
+++ basics/input_output.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+print(f"Hello, {name}")
+
+integer_num = int(input("Enter an integer: "))
+float_num = float(input("Enter a float: "))
+num_sum = integer_num + float_num
+num_product = integer_num * float_num
+print(f"sum of numbers: {num_sum}")
+print(f"product of numbers: {num_product}")
+
+sentence = input("Enter sentence: ")
+sentence_char = len(sentence)
+print(f"sentence characters: {sentence_char}")
