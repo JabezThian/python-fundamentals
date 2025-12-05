cfa4be81040df1997596dc7cb9c13e91eedf443f
JabezThian <thian.jabez@gmail.com>
2025-12-05 18:25:14 +0800

    questuion1 for nested_conditions

---
+++ conditions/nested_conditions.py
@@ -0,0 +1,25 @@
+# Question 1
+score = float(input("What is your current score? "))
+if score < 60:
+    print("F")
+
+
+
+if 90 <= score <= 100:
+    print("A")
+else:
+    if 80 <= score < 90:
+        print("B")
+    else:
+        if 70<= score < 80:
+            print("C")
+        else:
+            if 60 <= score < 70:
+                print("D")
+            else:
+                if 60 > score > 0:
+                    print("F")
+                else:
+                    print("Invalid grade")
+
+# Question 2:
